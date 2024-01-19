d142f50f3e8dac9056c1857ece5ca554f541b3f5
Tomas Kikutis <t.kikutis@gmail.com>
2024-01-19 09:44:19 +0100

    WithSortable - use hooks to improve code preview in storybook

--- src/ui/with-sortable.stories.tsx
+++ src/ui/with-sortable.stories.tsx
@@ -3,48 +3,6 @@ import type {Meta, StoryObj} from '@storybook/react';
 import {WithSortable} from './with-sortable';
 import {arrayMove} from '../data-structures/array-move';
 
-interface ITestItem {
-	name: string;
-}
-
-type IProps = {};
-
-interface IState {
-	items: Array<ITestItem>;
-}
-
-class Demo extends React.PureComponent<IProps, IState> {
-	constructor(props: IProps) {
-		super(props);
-
-		this.state = {
-			items: [
-				{name: 'item 1'},
-				{name: 'item 2'},
-				{name: 'item 3'},
-				{name: 'item 4'},
-				{name: 'item 5'},
-			],
-		};
-	}
-	render() {
-		return (
-			<WithSortable
-				items={this.state.items}
-				itemTemplate={({item}) => <div style={{margin: 4, border: '1px solid red'}}>{item.name}</div>}
-				getId={(item) => item.name}
-				options={{
-					onSortEnd: ({oldIndex, newIndex}) => {
-						this.setState({
-							items: arrayMove(this.state.items, oldIndex, newIndex),
-						});
-					},
-				}}
-			/>
-		);
-	}
-}
-
 const meta: Meta<typeof WithSortable> = {
 	title: 'HOC/WithSortable',
 	component: WithSortable,
@@ -62,5 +20,26 @@ export default meta;
 type Story = StoryObj<typeof WithSortable>;
 
 export const Main: Story = {
-	render: () => <Demo />,
+	render: () => {
+		const [items, setItems] = React.useState([
+			{name: 'item 1'},
+			{name: 'item 2'},
+			{name: 'item 3'},
+			{name: 'item 4'},
+			{name: 'item 5'},
+		]);
+
+		return (
+			<WithSortable
+				items={items}
+				itemTemplate={({item}) => <div style={{margin: 4, border: '1px solid red'}}>{item.name}</div>}
+				getId={(item) => item.name}
+				options={{
+					onSortEnd: ({oldIndex, newIndex}) => {
+						setItems(arrayMove(items, oldIndex, newIndex));
+					},
+				}}
+			/>
+		);
+	},
 };
